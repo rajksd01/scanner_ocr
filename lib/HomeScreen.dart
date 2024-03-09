d55febeeaac7e90e4694e6f8d3c6b7ecf05f9cfa
Raj <rajksd9@gmail.com>
2024-03-09 19:11:18 +0530

    App created

---
+++ lib/HomeScreen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:image_to_text/TextScanner.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => TextScanner()));
+          },
+          child: const Text('Open Camera to scan'),
+        ),
+      ),
+    );
+  }
+}
